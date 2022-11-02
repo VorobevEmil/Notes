dd7ced45b33af46f0420ed69ed52559408e4334d
Emil <vorobevemil@gmail.com>
2022-11-02 21:47:39 +0300

    IdentityServer4  Конфигурация

---
+++ Notes.Identity/Notes.Identity/Program.cs
@@ -0,0 +1,47 @@
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration["DbConnection"];
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlite(connectionString);
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScropes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
+
+app.Run();
